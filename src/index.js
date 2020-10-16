86ae003b8452a4b89c841a7a676b43fbc274a6d4
Ivan <Ivanestver24@gmail.com>
2020-10-16 16:52:12 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    if (expr.length % 10 === 0) {
+        for (let i = 0; i < expr.length; i += 10) {
+            let str = expr.slice(i, i + 10);
+            if (str === '**********') {
+                result += ' ';
+                continue;
+            }
+            let letter = '';
+            for (j = 0; j < str.length; j += 2) {
+                if (str[j] === '1') {
+                    if (str[j + 1] === '0') {
+                        letter += '.';
+                    }
+                    else {
+                        letter += '-';
+                    }
+                }
+            }
+            result += MORSE_TABLE[letter];
+        }
+    }
+
+    return result;
 }
 
 module.exports = {
